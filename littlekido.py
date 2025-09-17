645cde99cbb33808c980102a0b90bfb26f301c52
PRAJJWAL <prajjwal26082006@gmail.com>
2025-09-17 18:11:12 +0530

    Add files via upload

---
+++ littlekido.py
@@ -0,0 +1,9 @@
+name=input()
+age=int(input())
+if name=='Prajjwal' and age==20:
+    print('Hello , Prajjwal')
+elif age<20:
+    print('You are not Prajjwal Kiddo')
+else:
+    print('You are neither Prajjwal nor kiddo') 
+     
