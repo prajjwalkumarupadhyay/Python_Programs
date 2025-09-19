463ad2c0d86be7c8e91f430c6f6b9bcddbad1170
PRAJJWAL <prajjwal26082006@gmail.com>
2025-09-19 08:14:50 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,12 @@
+#Your Privacy is in your hand
+
+print('Enter Your Age')
+age=int(input())
+print('Enter Your Password')
+password=input()
+
+if password=='Prajjwal' and age==20:
+    print('Password is correct')
+
+else:
+    print('Mind your own business')
