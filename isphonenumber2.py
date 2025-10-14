02a479c7d7d4a1a76b243105dde3930ad300ff20
PRAJJWAL <prajjwal26082006@gmail.com>
2025-10-14 19:20:24 +0530

    Add files via upload

---
+++ isphonenumber2.py
@@ -0,0 +1,26 @@
+# This program checks if the input is a phone number or not 
+
+def isphonenumber(text):
+    if len(text)!=12:
+        return False
+    for i in range(1,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3]!='-':
+        return False
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+    if text[7]!='-':
+        return False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+  chunk = message[i:i+12]
+  if isphonenumber(chunk):
+    print('Phone number found: ' + chunk)
+print('Done')
