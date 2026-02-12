8f376589b2a17a1f3e435e419f09262f7060ad70
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-02-12 06:48:31 +0530

    uploded on 12/02/26

---
+++ compound.py
@@ -0,0 +1,6 @@
+print('Calculate simple interest ')
+p=int(input('Enter principle amount :\n'))
+t=int(input('Time:\n'))
+print('Interest Rate is 10%')
+c=p*(1+10/100)*t-p
+print('Compound Interest:\n',c)
