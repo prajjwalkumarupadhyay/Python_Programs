f66667af2f8a116aff8bc1c4333a363f47bf7c01
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-03-17 21:40:04 +0530

    Words Counter

--- wordscount.py
+++ wordscount.py
@@ -1,4 +1,16 @@
 sentence=input('Type a line or paragraph.\n')
 words=sentence.lower().split()
 words_sorted=sorted(words)
-print(words_sorted)
+#print(words_sorted)
+ind={}
+for i in range(len(words_sorted)):
+    word=words_sorted[i]
+    if word in ind:
+        x=ind[word]
+        x+=1
+        ind[word]=x
+    else:
+        ind[word]=1
+print('The following numbers indicates the repetition of that word in sentence or paragraph.')
+for key,value in ind.items():
+     print(key,'-',value)
