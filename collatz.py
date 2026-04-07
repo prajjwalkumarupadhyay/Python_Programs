bb3520626e0cd664c2e07ae9f3e1105e8a7a5c17
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-04-07 21:18:32 +0530

    Collatz

--- collatz.py
+++ collatz.py
@@ -1,12 +1,21 @@
 #This is collatz Program
-
-x=int(input())
-while x!=1:
-    if x%2==0:
-        x=x//2
-        print(x)
+import sys
+def main():
+    def collatz(number):
+        if number%2==0:
+            print(number//2)
+            return number//2
+        else:
+            print(3*number+1)
+            return 3*number+1
+    try:
+        user=int(input('Enter an integer.\n'))
+    except ValueError:
+        print('Error:Enter Correct Value:')
+        main()
+    value=collatz(user)
+    while value>1:
+        value=collatz(value)
     else:
-        x=3*x+1
-        print(x)
-
-print(x)
+        sys.exit()
+main() 
