bcb45ccc9c0e844abd49a6b0a7fac1a71a8ccf2f
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-06-15 20:47:55 +0530

    wcexamples2

---
+++ wcexamples2.py
@@ -0,0 +1,10 @@
+import Wizcoin
+change=Wizcoin.wWizCoin(90,11,20)
+print(change.sickles)
+change.sickles+=10
+print(change.sickles)
+
+pile=Wizcoin.wWizCoin(2,3,4)
+print(pile.sickles)
+pile.someNewAttribute='a new attr'
+print(pile.someNewAttribute)
